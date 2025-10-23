544a13cd89450e3c636e10e3a4e94c9a4fba756a
Nicholas Paun <npaun@cloudflare.com>
2025-10-23 13:18:31 -0700

    Provide a copy of EVP_CIPHER_do_all_sorted if required

--- tests/basic.cpp
+++ tests/basic.cpp
@@ -2,6 +2,20 @@
 
 #include <gtest/gtest.h>
 
-TEST(basic, test_it) {
-  SUCCEED();
+#include <string>
+#include <unordered_set>
+
+using namespace ncrypto;
+
+TEST(basic, cipher_foreach) {
+  std::unordered_set<std::string> foundCiphers;
+
+  Cipher::ForEach([&](const char* name) { foundCiphers.insert(name); });
+
+  // When testing Cipher::ForEach, we cannot expect a particular list of ciphers
+  // as that depends on openssl vs boringssl, versions, configuration, etc.
+  // Instead, we look for a couple of very common ciphers that should always be
+  // present.
+  ASSERT_TRUE(foundCiphers.count("AES-128-CTR"));
+  ASSERT_TRUE(foundCiphers.count("AES-256-CBC"));
 }
