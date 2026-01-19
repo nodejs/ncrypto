ca513cd305b9633ad6824a03242ec8ab9a911703
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-01-19 18:36:39 +0100

    fixup! chore: add Release Please GHA workflow

--- include/version.h
+++ include/version.h
@@ -5,14 +5,10 @@
 
 #define NCRYPTO_VERSION "1.0.1"  // x-release-please-version
 
-namespace ncrypto {
-
 enum {
   NCRYPTO_VERSION_MAJOR = 1,     // x-release-please-major
   NCRYPTO_VERSION_MINOR = 0,     // x-release-please-minor
   NCRYPTO_VERSION_REVISION = 1,  // x-release-please-patch
 };
-
-}  // namespace ncrypto
 
 #endif  // NCRYPTO_VERSION_H_
